53ee9bb08fe357078d770c8edc898c724a9ae5b3
kevinMG01 <montanokevin46@gmail.com>
2023-04-12 17:41:20 -0300

    limites de camara
    pusimos los limites de camra. no tocar hay mejores para poner limites de camara

--- Nivel_1/Nivel_1.gd
+++ Nivel_1/Nivel_1.gd
@@ -8,9 +8,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+#	get_tree().get_nodes_in_group("camara")[0].set_limit(MARGIN_LEFT, get_tree().get_nodes_in_group("min")[0].get_pos().x)
+#	get_tree().get_nodes_in_group("camara")[0].set_limit(MARGIN_TOP, get_tree().get_nodes_in_group("min")[0].get_pos().y)
+#	get_tree().get_nodes_in_group("camara")[0].set_limit(MARGIN_RIGHT, get_tree().get_nodes_in_group("max")[0].get_pos().x)
+#	get_tree().get_nodes_in_group("camara")[0].set_limit(MARGIN_BOTTOM, get_tree().get_nodes_in_group("max")[0].get_pos().y)
+	pass
+	
