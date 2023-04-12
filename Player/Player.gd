b6c7628c12c6433bb048cebbc760f1f2d59f016c
kevinMG01 <montanokevin46@gmail.com>
2023-04-12 17:59:26 -0300

    pawer up
    ya esta echo

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,12 +4,25 @@ extends KinematicBody2D
 var cantidad = 150
 var movimiento = Vector2()
 
+#powuer up
+var power_up = 0
+var cantidad_power = 280
+
 var gravedad = 70
 var masa = 2
 	
 func _physics_process(delta):
-	move()
-	
+	if power_up == 0:
+		move()
+	if power_up == 1:
+		move_pawer_up()
+		
+	if Input.is_action_just_pressed("ui_accept"):
+		power_up = 1
+		yield(get_tree().create_timer(1,5),"timeout")
+		power_up = 0
+		
+		
 	movimiento = move_and_slide(movimiento)
 	movimiento.y = gravedad * masa
 	#movimiento = movimiento.normalized()* cantidad
@@ -20,6 +33,14 @@ func move():
 	movimiento.x = 0
 	if Input.is_action_pressed("ui_left"):
 		movimiento.x -= cantidad
-		
+	
 	if Input.is_action_pressed("ui_right"):
 		movimiento.x += cantidad
+	
+func move_pawer_up():
+	movimiento.x = 0
+	if Input.is_action_pressed("ui_left"):
+		movimiento.x -= cantidad_power
+	
+	if Input.is_action_pressed("ui_right"):
+		movimiento.x += cantidad_power
