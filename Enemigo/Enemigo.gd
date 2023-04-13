ba6b568e5a37b1ca71951a38668ce2385f563ea1
kevinMG01 <montanokevin46@gmail.com>
2023-04-12 22:50:29 -0300

    segir al jugador
    enemigo

--- Enemigo/Enemigo.gd
+++ Enemigo/Enemigo.gd
@@ -6,12 +6,18 @@ var movimiento = Vector2()
 
 var jugador = null
 
+var jugador_abajo = null
+var cantidad_abajo = 180
+
+var nivel = 1
 
 
 func _physics_process(delta):
 	movimiento = Vector2()
-	
-	deteccion()
+	if nivel == 1:
+		deteccion()
+	if nivel == 2:
+		deteccion_seguir()
 	
 	#if jugador != null:
 	#	movimiento = position.direction_to(jugador.position)
@@ -27,16 +33,38 @@ func deteccion():
 			movimiento.x -= cantidad
 		if global_position.x < jugador.global_position.x - 6:
 			movimiento.x += cantidad
+		if global_position.y < jugador.global_position.x - 6:
+			movimiento.y += cantidad
+
+func deteccion_seguir():
 	
+	if jugador_abajo != null:
+		movimiento = position.direction_to(jugador_abajo.position)
+	else:
+		movimiento = Vector2()
+	
+	movimiento = movimiento.normalized()* cantidad_abajo
+	
+	pass
 
 func _on_Area2D_body_entered(body):
 	if body.get_name() == "Player":
 		jugador = body
 		
 		
-	pass # Replace with function body.
 
 
 func _on_Area2D_body_exited(body):
 	jugador = null
 	pass # Replace with function body.
+
+
+
+
+
+func _on_abajo_body_entered(body):
+	if body.get_name() == "Player":
+		jugador_abajo = body
+
+func _on_abajo_body_exited(body):
+	jugador_abajo = null
