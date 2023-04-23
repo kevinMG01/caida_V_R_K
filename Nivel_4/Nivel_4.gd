91336da3d24af9302190ce8899304a7efe5c5291
kevinMG01 <montanokevin46@gmail.com>
2023-04-23 15:15:22 -0300

    nivel4
    diseño

---
+++ Nivel_4/Nivel_4.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+
+var spawn_1 = true
+var player_1 = preload("res://Player/Player.tscn")
+
+
+func _physics_process(delta):
+	spawn_player()
+	
+
+
+func spawn_player():
+	if global_Var.camara_avion_apagado == false:
+		if spawn_1 == true:
+			var newplayer = player_1.instance()
+			add_child(newplayer)
+			newplayer.global_position = get_tree().get_nodes_in_group("spawn")[0].global_position
+			spawn_1 = false
