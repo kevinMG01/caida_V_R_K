a9d256d2772dfff1fe5292d38c04d7a204092cfa
kevinMG01 <montanokevin46@gmail.com>
2023-06-23 19:40:00 -0300

    zona_seleccion
    listo

--- Mundo/Seleccion_Zona/seleccion_Zona.gd
+++ Mundo/Seleccion_Zona/seleccion_Zona.gd
@@ -15,51 +15,46 @@ var scroll_to_index : int = 0 # animation tree
 
 func _ready():
 	yield(get_tree(), "idle_frame")
-	scroller.scroll_horizontal = rect_size.x * 2 #nosda el tamaaño de la ventana y mueve 2 posicciones mas
-	pass # Replace with function body.
+	scroller.scroll_horizontal = rect_size.x * 0 #nosda el tamaaño de la ventana y mueve 2 posicciones mas
+	pass 
 
 
 func _process(delta):
+	#indice var scroll = iiiiiiiiiiiiiiii. nos da como in indise
+	var scroll= scroller.scroll_horizontal/rect_size.x
 	
-	if is_manual_scrolling:
-		if abs(int(round(scroll_to))-scroller.scroll_horizontal) > 5:
-			animation_tree["parameters/blend_position"] = lerp(animation_tree["parameters/blend_position"],int(round(scroll_to/rect_size.x)),0.1)
-			scroller.scroll_horizontal = lerp(scroller.scroll_horizontal,scroll_to, 0.2)
-		else:
-			animation_tree["parameters/blend_position"] = scroll_to_index
-			scroller.scroll_horizontal = scroll_to
-			is_manual_scrolling = false
-			set_process(false)
+	if is_scrolling:
+		animation_tree["parameters/blend_position"] = scroll 
+		
+	elif abs(int(round(scroll))-scroll) > 0.01:
+		animation_tree["parameters/blend_position"] = lerp(animation_tree["parameters/blend_position"],int(round(scroll)),0.1)
+		scroller.scroll_horizontal = lerp(scroller.scroll_horizontal,rect_size.x * int(round(scroll)), 0.2)
 	else:
-		var scroll = scroller.scroll_horizontal/rect_size.x # nos da como un indise, sabremos en que posicion bamos a paserle el scroler, para la animacion
-		if is_scrolling:
-			animation_tree["parameters/blend_position"] = scroll
-		elif abs(int(round(scroll))-scroll) > 0.01:
-			animation_tree["parameters/blend_position"] = lerp(animation_tree["parameters/blend_position"],int(round(scroll)),0.1)
-			scroller.scroll_horizontal = lerp(scroller.scroll_horizontal,rect_size.x * int(round(scroll)), 0.2)
-		else:
-			animation_tree["parameters/blend_position"] = int(round(scroll))
-			scroller.scroll_horizontal = rect_size.x * int(round(scroll))
-			set_process(false)
+		animation_tree["parameters/blend_position"] = int(round(scroll))
+		scroller.scroll_horizontal = rect_size.x * int(round(scroll))
+		set_process(false)
+	#nose porque no da el "print()" seguro hay un error. fijarse mas tarde
+	if scroller.scroll_horizontal == rect_size.x * 3:
+		print("hola muundo")
 	pass
 
 
 
+#scroll manual
+func _on_ScrollContainer_scroll_ended():
+	is_scrolling = false
+	pass
 
+func _on_ScrollContainer_scroll_started():
+	is_scrolling = true
+	set_process(true)
+	pass
 
+#Boton_scroll
+func _on_Atras_pressed():
+	scroller.scroll_horizontal -= rect_size.x * 1
+	pass # Replace with function body.
 
-#scroll_to = (rect_size.x * extra_arg_0)
-#	scroll_to_index = extra_arg_0
-#	is_manual_scrolling = true
-#	set_process(true)
-
-
-
-
-
-
-
-
-
-
-
+func _on_Adelante_pressed():
+	scroller.scroll_horizontal += rect_size.x * 1
+	pass # Replace with function body.
