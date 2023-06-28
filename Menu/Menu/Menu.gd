cde766d34fa166553b3c2540b244801e6834138f
kevinMG01 <montanokevin46@gmail.com>
2023-06-27 21:01:17 -0300

    jad
    as

---
+++ Menu/Menu/Menu.gd
@@ -0,0 +1,41 @@
+extends Control
+
+
+
+
+func _on_jugar_pressed():
+	get_tree().change_scene("res://Mundo/Seleccion_Zona/seleccion_Zona.tscn")
+	pass 
+
+
+func _on_Salir_pressed():
+	get_tree().quit()
+	pass 
+
+
+func _on_Button3_pressed():
+	get_tree().change_scene("res://Mundo/Zonas/Zona_1/Nivel_1.tscn")
+	pass 
+
+
+func _on_Tienda_pressed():
+	get_tree().change_scene("res://Mundo/Zonas/Zona_1/Nivel_1.tscn")
+	pass 
+
+
+func _on_Inventario_pressed():
+	get_tree().change_scene("res://Mundo/Zonas/Zona_1/Nivel_1.tscn")
+	pass 
+
+
+func _on_Biografia_pressed():
+	get_tree().change_scene("res://Mundo/Zonas/Zona_1/Nivel_1.tscn")
+	
+	pass 
+
+
+
+
+
+
+
