62c5fd398580bc730e865496b440ee5e9dcad7b6
kevinMG01 <montanokevin46@gmail.com>
2023-06-30 20:53:33 -0300

    s
    as

--- Menu/Menu/Menu.gd
+++ Menu/Menu/Menu.gd
@@ -12,21 +12,13 @@ func _on_Salir_pressed():
 	get_tree().quit()
 	pass 
 
-
-func _on_Button3_pressed():
-	get_tree().change_scene("res://Mundo/Zonas/Zona_1/Nivel_1.tscn")
-	pass 
-
-
 func _on_Tienda_pressed():
-	get_tree().change_scene("res://Mundo/Zonas/Zona_1/Nivel_1.tscn")
+	get_tree().change_scene("")
 	pass 
-
 
 func _on_Inventario_pressed():
 	get_tree().change_scene("")
 	pass 
-
 
 func _on_Biografia_pressed():
 	get_tree().change_scene("res://Menu/Controles/Biografia.tscn")
