cd605a38458769e859c3f42a09ef2b1b50062ab8
kevinMG01 <montanokevin46@gmail.com>
2023-07-19 17:58:47 -0300

    nivel 2
    en

--- Nivel_2.gd
+++ Nivel_2.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 var spawn_1 = true
 var player_1 = preload("res://Player/Player.tscn")
 
@@ -7,7 +8,9 @@ func _ready():
 	global_Var.camara_avion_apagado = true
 
 func _physics_process(delta):
+#	$Interfas_pantalla.rect_global_position = get_tree().get_nodes_in_group("camara")[0].global_position
 	spawn_player()
+	camara_nivel_1()
 	
 	pass
 	
@@ -24,4 +27,12 @@ func _on_Detener_enemigo_body_entered(body):
 	if body.is_in_group("enemigo"):
 		global_Var.deteccion_enemigo = false
 
+func camara_nivel_1():
+	if global_Var.camara_avion_apagado == true:
+		$Camera2D.global_position = get_tree().get_nodes_in_group("avion")[0].global_position
+	elif global_Var.camara_avion_apagado == false:
+		$Camera2D.global_position = get_tree().get_nodes_in_group("player")[0].global_position
 
+
+#func _process(delta):
+#	pass
