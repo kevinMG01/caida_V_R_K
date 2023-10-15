48afa1a9ff059e02379c7d6050cb90111936f045
kevinMG01 <montanokevin46@gmail.com>
2023-10-15 12:39:43 -0300

    s
    s

--- Mundo/Zonas/inicio.gd
+++ Mundo/Zonas/inicio.gd
@@ -4,10 +4,10 @@ extends Node2D
 
 
 
-var en_pantalla = 0
+var en_pantalla = 1
 
 func _ready():
-	en_pantalla = 0
+	en_pantalla = 1
 	pass # Replace with function body.
 
 
@@ -19,9 +19,6 @@ func _physics_process(delta):
 		$AnimatedSprite.animation = "1"
 		pass
 	if en_pantalla == 2:
-		$AnimatedSprite.animation = "2"
-		pass
-	if en_pantalla == 3:
 		get_tree().change_scene("res://Mundo/Zonas/Zona_1/Nivel_1.tscn")
 
 		pass
